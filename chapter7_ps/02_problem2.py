b18904a485cfa3e7e720892badf855de45a3ac15
mushfika <musfika962@gmail.com>
2026-09-10 21:01:41 +0600

    add this commit

---
+++ chapter7_ps/02_problem2.py
@@ -0,0 +1,5 @@
+l = ["Harry", "Shumbham", "Rohit", "Rohan"]
+
+for name in l:
+    if(name.startswith("R")):
+        print(f"Hello, {name}")
---
+++ chapter7_ps/03_problem3.py
@@ -0,0 +1,6 @@
+number = int(input("Enter a number: "))
+i = 0
+
+while(i < 11):
+    print(f"{number} X {i} = {number * i}")
+    i += 1
