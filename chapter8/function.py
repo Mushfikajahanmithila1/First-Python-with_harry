1528efdbc31dfdb32a6f59d448416b0246d95d7f
mushfika <musfika962@gmail.com>
2026-09-11 16:44:49 +0600

    take

---
+++ chapter8/function.py
@@ -0,0 +1,9 @@
+# function definition
+def avg():
+    a = int(input("Enter your first number: "))
+    b = int(input("Enter your second number: "))
+    c = int(input("Enter your third number: "))
+    print("The average is:", (a + b + c) / 3)
+
+# function call
+avg()
